d97d60e1c8479b599a8b40ac7d70b89a91c8ba05
Vitstdsuba21 <144683114+Vitstdsuba21@users.noreply.github.com>
2025-04-09 13:32:07 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,8 @@ app_mode = st.sidebar.selectbox("Select Page", ["Home", "About", "Disease Recogn
 if app_mode == "Home":
     st.header("PLANT DISEASE RECOGNITION SYSTEM")
     image_path = "home_page.jpg"
-    st.image(image_path, use_column_width=True)
+    st.image(image_path, use_container_width=True)
+
     st.markdown(""" 
     Welcome to the Plant Disease Recognition System! 🌿🔍
     
